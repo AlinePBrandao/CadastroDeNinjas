fab964f0a5467b9e19cfef82f7ff95d787730fb9
alinebrandao <alinep02brandao@gmail.com>
2026-03-10 23:14:14 -0300

    first commit

---
+++ src/main/java/project/java/cadastrodeninjas/SistemaDeCadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package project.java.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCadastroDeNinjasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeCadastroDeNinjasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/java/cadastrodeninjas/SistemaDeCadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package project.java.cadastrodeninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroDeNinjasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
