a6db54c576ccd8ce8ddc59c624f0204f90f1981b
alinebrandao <alinep02brandao@gmail.com>
2026-03-16 18:10:57 -0300

    criando primeira rota da API

---
+++ src/main/java/project/java/cadastrodeninjas/Controller.java
@@ -0,0 +1,15 @@
+package project.java.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas") //NOTE: passando uma rota
+    public String boasvindas(){
+        return "Boas Vindas! Essa é minha primeira mensagem nessa rota";
+    }
+}
