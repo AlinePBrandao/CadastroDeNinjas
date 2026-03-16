16449bf27bbd05af84e87b55b272e0b4d2339a6f
alinebrandao <alinep02brandao@gmail.com>
2026-03-16 20:28:35 -0300

    criando classe ninja contendo atributos, default constructor e constructor com argumentos

---
+++ src/main/java/project/java/cadastrodeninjas/NinjaModel.java
@@ -0,0 +1,17 @@
+package project.java.cadastrodeninjas;
+
+public class NinjaModel {
+
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
