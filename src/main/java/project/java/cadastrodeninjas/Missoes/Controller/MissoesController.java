5eb08c40119ea97ca8a5e54737099e5e836f3202
alinebrandao <alinep02brandao@gmail.com>
2026-07-09 18:37:13 -0300

    feat: Adiciona lógica do Controller para criar ninjas e injeta dependência do Service e do Repository

--- src/main/java/project/java/cadastrodeninjas/Missoes/Controller/MissoesController.java
+++ src/main/java/project/java/cadastrodeninjas/Missoes/Controller/MissoesController.java
@@ -1,15 +1,22 @@
 package project.java.cadastrodeninjas.Missoes.Controller;
 
 import org.springframework.web.bind.annotation.*;
+import project.java.cadastrodeninjas.Missoes.Model.MissoesModel;
+import project.java.cadastrodeninjas.Missoes.Service.MissoesService;
 
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
-    //Adicionar missão
-    @PostMapping("/create") //envia informações
-    public String criateNinja(){
-        return "Missão Criada";
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService){
+        this.missoesService = missoesService;
+    }
+
+    @PostMapping("/createMissao") //envia informações
+    public MissoesModel createMissao(@RequestBody MissoesModel model){
+        return missoesService.createMissao(model);
     }
 
     //Mostrar missões
--- src/main/java/project/java/cadastrodeninjas/Missoes/Service/MissoesService.java
+++ src/main/java/project/java/cadastrodeninjas/Missoes/Service/MissoesService.java
@@ -1,4 +1,17 @@
 package project.java.cadastrodeninjas.Missoes.Service;
 
+import project.java.cadastrodeninjas.Missoes.Model.MissoesModel;
+import project.java.cadastrodeninjas.Missoes.Repository.MissoesRepository;
+
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository){
+        this.missoesRepository = missoesRepository;
+    }
+
+    public MissoesModel createMissao(MissoesModel model){
+        return missoesRepository.save(model);
+    }
 }
