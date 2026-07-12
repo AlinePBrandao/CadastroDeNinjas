64df0f137d016b80230559913d710d3103891149
alinebrandao <alinep02brandao@gmail.com>
2026-07-12 00:34:27 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no MissoesMapper os caminhos entidade x dto

--- src/main/java/project/java/cadastrodeninjas/Missoes/DTO/MissoesDTO.java
+++ src/main/java/project/java/cadastrodeninjas/Missoes/DTO/MissoesDTO.java
@@ -1,4 +1,16 @@
 package project.java.cadastrodeninjas.Missoes.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesDTO {
+
+    private long id;
+    private String nome;
+    private String dificuldade;
+    private String descricao;
 }
--- src/main/java/project/java/cadastrodeninjas/Missoes/Mapper/MissoesMapper.java
+++ src/main/java/project/java/cadastrodeninjas/Missoes/Mapper/MissoesMapper.java
@@ -1,4 +1,27 @@
 package project.java.cadastrodeninjas.Missoes.Mapper;
 
+import org.springframework.stereotype.Component;
+import project.java.cadastrodeninjas.Missoes.DTO.MissoesDTO;
+import project.java.cadastrodeninjas.Missoes.Model.MissoesModel;
+
+@Component
 public class MissoesMapper {
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setDescricao(missoesDTO.getDescricao());
+
+        return missoesModel;
+    }
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesDTO.getId());
+        missoesDTO.setNome(missoesDTO.getNome());
+        missoesDTO.setDificuldade(missoesDTO.getDificuldade());
+        missoesDTO.setDescricao(missoesDTO.getDescricao());
+
+        return missoesDTO;
+    }
 }
